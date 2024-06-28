008f66b6c2194330829f6a5b3558747cf73021b3
Anokhin <a.i.anohin.arduino@gmail.com>
2024-06-28 14:38:54 +0300

    Init commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.printf("Сравнение бинов helloworld. Бины равны? %s", bean == bean2);
     }
 }
