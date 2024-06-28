798fb4d454c7cc8efb30a836ee8ced641f8a36ad
Anokhin <a.i.anohin.arduino@gmail.com>
2024-06-28 15:07:39 +0300

    DONE

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,25 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
+        //так как у бина "helloworld" "Scope = Singleton" (тоесть по умолчанию),
+        // то bean2 == bean
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean2.getMessage());
 
-        System.out.printf("Сравнение бинов helloworld. Бины равны? %s", bean == bean2);
+        System.out.printf("Сравнение бинов helloworld. Бины равны? %s\n", bean == bean2);
+
+        // В AppConfig мы присвоили бину, который создает объект CAT
+        // "Scope = Prototype", то каждая переменная будет ссылаться на свой объект "Cat"
+        // об этом говорит нам проверки на == и выведенное поле "name" объекта
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        bean3.setName("Musya");
+        System.out.println(bean3.getName());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        bean4.setName("Barsik");
+        System.out.println(bean4.getName());
+
+        System.out.printf("Сравнение бинов helloworld. Бины равны? %s", bean3 == bean4);
+
     }
 }
